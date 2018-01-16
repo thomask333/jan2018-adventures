c1b23cba1d9379149c4d6a152a21b14cbe1ebb32
Thomas King <thomasking.gs@outlook.com>
2018-01-16 08:43:06 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :activities
+  resources :states
+  root 'welcome#index'
+  get "ilovetocode"=>  "welcome#index"
+  get "about" => 'welcome#about'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
